450fee1a1a11b2f6a74b94652ac019d8a5bb55db
Norm Evangelista <norm.evangelista@ff.com>
2018-08-28 15:03:07 -0700

    updated install dependencies

--- InstallDependencies.py
+++ InstallDependencies.py
@@ -6,5 +6,5 @@ if platform == "win32":
 	subprocess.call('pip install intelhex'.split())
 	
 elif platform == "linux" or platform == "linux2":
-	subprocess.call('sudo python pip install progressbar'.split())
-	subprocess.call('sudo python pip install intelhex'.split())
+	subprocess.call('sudo pip install progressbar'.split())
+	subprocess.call('sudo pip install intelhex'.split())
